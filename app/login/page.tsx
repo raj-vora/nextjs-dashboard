d42d0b88b4bc4c4b07bc046b8354be23afaf3027
Raj Vora <rajvora@ufl.edu>
2025-03-22 12:19:18 -0400

    complete the course

---
+++ app/login/page.tsx
@@ -0,0 +1,20 @@
+
+import LoginForm from '@/app/ui/login-form';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+    title: 'Login',
+};
+
+export default function LoginPage() {
+    return (
+        <main className="flex items-center justify-center md:h-screen">
+            <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+                <Suspense>
+                    <LoginForm />
+                </Suspense>
+            </div>
+        </main>
+    );
+}
